831e14f11f094dbe488f5fe507d24f96e5a6d534
Nirmal Prasad <137271354+Niru2k@users.noreply.github.com>
2023-09-27 15:51:42 +0530

    hello world

---
+++ main.go
@@ -0,0 +1,11 @@
+// Here is an example code in the Go programming language.
+// This code simply prints out the message "Hello World" to the console when it is executed.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World")
+}
